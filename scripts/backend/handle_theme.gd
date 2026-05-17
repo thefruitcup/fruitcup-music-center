0473aaf0afe82e658849d350b228b3e2e8ef7dde
carlosfruitcup <40542088+carlosfruitcup@users.noreply.github.com>
2026-05-16 23:15:38 -0400

    Assets Folder Organized, Fallback Font, "Linux Support"
    - Organized Assets Folder
    - Added Noto Sans as Fallback for Non-Windows 10 Systems
    - Added Noto Sans License to FMC Attribution Screen & Attributions.md
    - Added Commands to swap between Segoe UI & Noto Sans
    - Added Linux Export Preset (Lite-Linux Support, I cannot fully support it with no Linux Machine)

---
+++ scripts/backend/handle_theme.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var windows_font :SystemFont= preload("res://assets/misc/windows_font.tres")
+@onready var fallback_font :FontFile= preload("res://assets/misc/NotoSans-Regular.ttf")
+
+func _ready() -> void:
+	Console.add_command("set_window_font",set_font.bind(windows_font))
+	Console.add_command("set_fallback_font",set_font.bind(fallback_font))
+	
+	if OS.get_name() == "Windows": return
+	set_font(fallback_font)
+
+func set_font(font : Font) -> void:
+	theme.default_font = font
+	theme.set("Button/fonts/font",font)
